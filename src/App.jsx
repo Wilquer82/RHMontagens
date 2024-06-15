1cc25b716210e56a973492de4b2c255dd7846d62
wilquer82 <wilquer.fig@gmail.com>
2024-06-15 15:26:29 -0300

    google4

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import RHLogo from './assets/RHMONTAGENSLOGO.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="main">
+      <header style={{
+        display:"flex", 
+        position: "fixed", 
+        top: "0", 
+        left: "0",
+        margin: "0 1rem",
+        justifyContent:"space-between",
+        width: "98%",
+        }}>
+          <img src={RHLogo} className="logo" alt="RHlogo" />
+          <h2 className="logo" >Nossa História</h2>
+          <h2 className="logo" >Atuação</h2>
+          <h2 className="logo" >Serviços</h2>
+          <h2 className="logo" >Contato</h2>
+      </header>
+      <h1>RH Montagens Industriais</h1>
+
+    </div>
   )
 }
 
